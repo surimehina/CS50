1eb74fe6a30671edbc0bafdc6d15e8f4e81fec62
surimehina <1257983246@qq.com>
2023-02-23 15:04:55 +0800

    coke

--- week2/coke.py
+++ week2/coke.py
@@ -1,24 +1,13 @@
 def main():
     print("Amount Due: 50")
-    is_enough()
-
-def is_enough():
     all_money = []
-    while True:
+    while sum(all_money) < 50:
         money = int(input("Insert coin: "))
-        temp = 0
+        if money in [5, 10, 25]:
+            all_money.append(money)
+        print("Amount Due: ", sum(all_money))
+    print("Change Owed: ", sum(all_money) - 50)
+        
 
-        if money > 25:
-            money = 0
-            money = money - temp
-
-        all_money.append(money)
-        sum_money = sum(all_money)
-        print("Amount Due: ", sum_money)
-
-        if sum_money >= 50:
-            print("Change Own: ", abs(50 - sum_money) + temp)
-            break            
-# 原测试例似乎存在问题，在投入25之后再投入10，例子中还要投入的钱居然变成40了
 
 main()
