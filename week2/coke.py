2d7d1ddcafebb7e74c4feea8c5cc2f2a3aa4534f
surimehina <1257983246@qq.com>
2023-02-23 14:39:54 +0800

    coke

---
+++ week2/coke.py
@@ -0,0 +1,24 @@
+def main():
+    print("Amount Due: 50")
+    is_enough()
+
+def is_enough():
+    all_money = []
+    while True:
+        money = int(input("Insert coin: "))
+        temp = 0
+
+        if money > 25:
+            temp = money - 5
+            money = money - temp
+
+        all_money.append(money)
+        sum_money = sum(all_money)
+        print("Amount Due: ", sum_money)
+
+        if sum_money >= 50:
+            print("Change Own: ", abs(50 - sum_money) + temp)
+            break            
+# 原测试例似乎存在问题，在投入25之后再投入10，例子中还要投入的钱居然变成40了
+
+main()
